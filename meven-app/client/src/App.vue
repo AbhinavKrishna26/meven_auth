20fabb5ff88985d1774932aa22d5c95281e49065
AbhinavKrishna26 <abhinavkrishna26chemistry@gmail.com>
2020-03-26 21:44:04 +0530

    third

--- meven-app/client/src/App.vue
+++ meven-app/client/src/App.vue
@@ -4,9 +4,9 @@
     <!-- <div id="nav"> -->
       <!-- <router-link to="/">Home</router-link> | -->
       <!-- <> -->
-    <!-- </div> -->
+    <!-- </div> --><div class="container">
     <router-view/>
-  </div>
+  </div></div>
 </template>
 
 <script>
