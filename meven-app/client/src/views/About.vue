bb10a0be52cde95f99d21a495170751ab835373f
AbhinavKrishna26 <abhinavkrishna26chemistry@gmail.com>
2020-03-29 23:27:38 +0530

    6th

--- meven-app/client/src/views/About.vue
+++ meven-app/client/src/views/About.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <header class="header">
+    <h1>TO-DO APP ON MEVN STACK</h1>
+   <p>
+     This app is designed by Abhinav Krishna</p>
+  </header>
+  
 </template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+  .header {
+    background: #333;
+    color: #fff;
+    text-align: center;
+    padding: 10px;
+  }
+  .header a {
+    color: rgb(228, 35, 35);
+    padding-right: 5px;
+    text-decoration: none;
+  }
+  
+</style>
