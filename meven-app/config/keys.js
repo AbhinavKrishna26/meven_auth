344857f5330905752ec653940672f7349f85e6a2
AbhinavKrishna26 <abhinavkrishna26chemistry@gmail.com>
2020-03-30 22:36:41 +0530

    newww

--- meven-app/config/keys.js
+++ meven-app/config/keys.js
@@ -1,13 +1,7 @@
 
-if(Process.env.NODE_ENV === 'production'){
-    module.exports = { mongoURI: "mongodb://localhost:27017/meven_auth",
-        secret: 'yoursecret'
-    };
-    
-}else {
+
     module.exports = {
         mongoURI: "mongodb://localhost:27017/meven_auth",
         secret: 'yoursecret'
     };
     
-}
